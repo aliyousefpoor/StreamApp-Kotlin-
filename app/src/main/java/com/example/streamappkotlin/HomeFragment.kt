78871b1799873cea5696f700efeb33029001c37a
Ali Yousefpoor <Ali@Mahdis-MacBook-Pro.local>
2020-09-15 00:26:21 +0430

    add recyclerview & ... to home layout

--- app/src/main/java/com/example/streamappkotlin/HomeFragment.kt
+++ app/src/main/java/com/example/streamappkotlin/HomeFragment.kt
@@ -4,9 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 
 class HomeFragment : Fragment() {
+    lateinit var arrow: ImageView
+    lateinit var recyclerView: RecyclerView
+    lateinit var swipeRefreshLayout: SwipeRefreshLayout
+    lateinit var pullDown: TextView
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -17,5 +26,10 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        arrow = view.findViewById(R.id.arrow)
+        pullDown=view.findViewById(R.id.pullDown)
+        recyclerView=view.findViewById(R.id.rec_view)
+        swipeRefreshLayout=view.findViewById(R.id.homeSwipeRefreshing)
     }
+
 }
