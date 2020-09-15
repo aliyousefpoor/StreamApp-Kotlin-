606693bd72b830868e592c7be0cba6720ff92e1b
Ali Yousefpoor <Ali@Mahdis-MacBook-Pro.local>
2020-09-15 05:22:35 +0430

    Add MultipleAdapter for Home

--- app/src/main/java/com/example/streamappkotlin/hometab/HomeFragment.kt
+++ app/src/main/java/com/example/streamappkotlin/hometab/HomeFragment.kt
@@ -9,13 +9,12 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.streamappkotlin.R
 import androidx.recyclerview.widget.RecyclerView
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 
 class HomeFragment : Fragment() {
     lateinit var arrow: ImageView
     lateinit var recyclerView: RecyclerView
-    lateinit var swipeRefreshLayout: SwipeRefreshLayout
     lateinit var pullDown: TextView
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,6 +29,5 @@ class HomeFragment : Fragment() {
         arrow = view.findViewById(R.id.arrow)
         pullDown=view.findViewById(R.id.pullDown)
         recyclerView=view.findViewById(R.id.rec_view)
-        swipeRefreshLayout=view.findViewById(R.id.homeSwipeRefreshing)
     }
 }
---
+++ app/src/main/java/com/example/streamappkotlin/hometab/MultipleAdapter.kt
@@ -0,0 +1,75 @@
+package com.example.streamappkotlin.hometab
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.streamappkotlin.R
+
+class MultipleAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private var viewPagerType: Int = 1
+    private var horizontalListType: Int = 2
+    private var context: Context
+
+    constructor(context: Context) {
+        this.context = context
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (position == 0) {
+            viewPagerType
+        } else {
+            horizontalListType
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        var holder: RecyclerView.ViewHolder? = null
+        var inflater: LayoutInflater = LayoutInflater.from(context)
+        when (viewType) {
+            viewPagerType -> {
+                var viewPager: View = inflater.inflate(R.layout.header_item_layout, parent, false)
+                holder = ViewPagerViewHolder(viewPager)
+                true
+            }
+            horizontalListType -> {
+                var listView: View = inflater.inflate(R.layout.home_item_layout, parent, false)
+                holder = ListViewHolder(listView)
+                true
+            }
+        }
+        return holder
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (getItemViewType(position)) {
+            viewPagerType -> {
+                var pagerHolder: ViewPagerViewHolder = holder as ViewPagerViewHolder
+                pagerHolder.onBind()
+                true
+            }
+            horizontalListType -> {
+                var listHolder: ListViewHolder = holder as ListViewHolder
+                listHolder.onBind()
+                true
+            }
+        }
+    }
+
+    class ViewPagerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun onBind() {
+
+        }
+    }
+
+    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun onBind() {
+
+        }
+    }
+}
