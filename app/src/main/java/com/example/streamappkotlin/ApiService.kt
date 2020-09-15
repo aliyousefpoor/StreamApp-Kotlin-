87ab18bb42df8703996d0d2c73f56f1de2cbe324
Ali Yousefpoor <Ali@Mahdis-MacBook-Pro.local>
2020-09-16 03:55:58 +0430

    Add CategoryModel

--- app/src/main/java/com/example/streamappkotlin/ApiService.kt
+++ app/src/main/java/com/example/streamappkotlin/ApiService.kt
@@ -1,4 +1,9 @@
 package com.example.streamappkotlin
 
+import com.example.streamappkotlin.model.Category
+import retrofit2.Call
+import retrofit2.http.GET
+
 interface ApiService {
+    @GET("category/16/463") fun getCategory(): Call<List<Category>>
 }
---
+++ app/src/main/java/com/example/streamappkotlin/model/Avatar.kt
@@ -0,0 +1,34 @@
+package com.example.streamappkotlin.model
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+class Avatar {
+    @SerializedName("xxxdpi")
+    @Expose
+    private lateinit var xxxdpi: String
+
+    @SerializedName("xxhdpi")
+    @Expose
+    private lateinit var xxhdpi: String
+
+    @SerializedName("xhdpi")
+    @Expose
+    private lateinit var xhdpi: String
+
+    @SerializedName("hdpi")
+    @Expose
+    private lateinit var hdpi: String
+
+    @SerializedName("mdpi")
+    @Expose
+    private lateinit var mdpi: String
+
+    fun getXhdpi(): String {
+        return "https://api.vasapi.click/$xhdpi"
+    }
+
+    fun setXhdpi(xhdpi: String) {
+        this.xhdpi = xhdpi
+    }
+}
