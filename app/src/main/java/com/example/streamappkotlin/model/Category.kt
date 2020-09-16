2f2895067d5e0c22f9c3b5a9ba08a0e0785c4abb
Ali Yousefpoor <Ali@Mahdis-MacBook-Pro.local>
2020-09-16 15:12:13 +0430

    complete category data class

--- app/src/main/java/com/example/streamappkotlin/model/Category.kt
+++ app/src/main/java/com/example/streamappkotlin/model/Category.kt
@@ -1,4 +1,12 @@
 package com.example.streamappkotlin.model
 
-data class Category(var id:Int,var title:String,var avatar:String,var position:Int,var isDefault: Boolean,var isEnable: Boolean
-,var parent: Int)
+data class Category(
+    var id: Int,
+    var title: String,
+    var avatar: String,
+    var position: Int,
+    var is_default: Boolean,
+    var is_enable: Boolean,
+    var is_visible: Boolean,
+    var parent: Int
+)
