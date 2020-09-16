db56aebcf0bf0a661d2605379145934a14aed0e3
Ali Yousefpoor <Ali@Mahdis-MacBook-Pro.local>
2020-09-16 04:52:18 +0430

    Add ViewModel LiveData to Fragment

--- app/src/main/java/com/example/streamappkotlin/categorytab/CategoryFragment.kt
+++ app/src/main/java/com/example/streamappkotlin/categorytab/CategoryFragment.kt
@@ -4,15 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.example.streamappkotlin.CustomApp
 import com.example.streamappkotlin.R
 import com.example.streamappkotlin.categorytab.di.CategoryTabModule
 import com.example.streamappkotlin.di.ApiBuilderModule
+import com.example.streamappkotlin.model.Category
 
 class CategoryFragment : Fragment() {
+    private lateinit var arrow: ImageView
+    private lateinit var pullDown: TextView
+    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
+    private lateinit var recyclerView: RecyclerView
     private var categoryViewModel: CategoryViewModel? = null
     private var retrofit = CustomApp.instance.appModule.provideRetrofit()
     private var apiBuilder = ApiBuilderModule.provideApiBuilder(retrofit)
@@ -32,6 +42,50 @@ class CategoryFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         categoryViewModel =
             ViewModelProviders.of(this, categoryViewModelFactory).get(CategoryViewModel::class.java)
+        arrow = view.findViewById(R.id.cat_arrow)
+        pullDown = view.findViewById(R.id.pullDown)
+        swipeRefreshLayout = view.findViewById(R.id.refreshing)
+        recyclerView = view.findViewById(R.id.recycler_view)
 
+        swipeRefreshLayout.setOnRefreshListener {
+            categoryViewModel!!.getCategory()
+        }
+
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        pullDown.visibility = View.GONE
+        arrow.visibility = View.GONE
+        swipeRefreshLayout.isRefreshing = true
+
+        categoryViewModel?.loadingLiveData?.observe(viewLifecycleOwner, Observer {
+            if (it == true) {
+                pullDown.visibility = View.GONE
+                arrow.visibility = View.GONE
+                recyclerView.visibility = View.GONE
+                swipeRefreshLayout.isRefreshing = true
+            } else {
+                pullDown.visibility = View.GONE
+                arrow.visibility = View.GONE
+                recyclerView.visibility = View.VISIBLE
+                swipeRefreshLayout.isRefreshing = false
+            }
+        })
+        categoryViewModel?.errorLiveData?.observe(viewLifecycleOwner, Observer {
+            pullDown.visibility = View.VISIBLE
+            arrow.visibility = View.VISIBLE
+            recyclerView.visibility = View.GONE
+            swipeRefreshLayout.isRefreshing = false
+            Toast.makeText(context, "Check Your Conecction !", Toast.LENGTH_SHORT).show();
+        })
+        categoryViewModel?.categoryList?.observe(viewLifecycleOwner, Observer {
+
+            showCategoryList(it)
+        })
+    }
+
+    private fun showCategoryList(response: List<Category>) {
+        var categoryList: List<Category> = response
     }
 }
