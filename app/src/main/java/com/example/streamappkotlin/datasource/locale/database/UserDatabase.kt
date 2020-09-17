1e753fc844061148ae47e91b505d7158d23b3d1f
Ali Yousefpoor <Ali@Mahdis-MacBook-Pro.local>
2020-09-17 04:50:39 +0430

    add UserDatabase

---
+++ app/src/main/java/com/example/streamappkotlin/datasource/locale/database/UserDatabase.kt
@@ -0,0 +1,24 @@
+package com.example.streamappkotlin.datasource.locale.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.streamappkotlin.datasource.locale.model.UserEntity
+
+@Database(entities = [UserEntity::class], exportSchema = false, version = 1)
+abstract class UserDatabase : RoomDatabase() {
+    companion object {
+        private val DB_Name: String = "user_db"
+        private var instance: UserDatabase? = null
+         fun getInstance(context: Context):UserDatabase{
+            if (instance==null){
+                instance= Room.databaseBuilder(context.applicationContext,UserDatabase::class.java,
+                    DB_Name).fallbackToDestructiveMigration().build()
+            }
+             return instance as UserDatabase
+        }
+        fun userDao() {}
+    }
+
+}
