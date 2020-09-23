37d30f8e997316fae2f0f3d5ccfc7d0a18a0503e
Ali Yousefpoor <Ali@Mahdis-MBP.lan>
2020-09-23 16:12:55 +0330

    add Category Api

--- app/src/main/java/com/example/streamappkotlin/ApiService.kt
+++ app/src/main/java/com/example/streamappkotlin/ApiService.kt
@@ -1,5 +1,6 @@
 package com.example.streamappkotlin
 
+import com.example.streamappkotlin.model.Category
 import com.example.streamappkotlin.model.Store
 import retrofit2.Call
 import retrofit2.http.GET
@@ -7,4 +8,5 @@ import retrofit2.http.GET
 
 interface ApiService {
     @GET("store/16") fun getStore():Call<Store>
+    @GET("category/16/463") fun getCategory(): Call<List<Category>>
 }
